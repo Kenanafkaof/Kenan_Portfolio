d1bcfe73fc869ac84f9ec51b411acf2963ae1396
Kenan Korn <70501296+Kenanafkaof@users.noreply.github.com>
2021-12-24 12:05:03 -0700

    Additions

---
+++ email.js
@@ -0,0 +1,38 @@
+(function () {
+    emailjs.init("user_tXNhh0xg6IHcxwjxVkxE1");
+})();
+function hide (elements) {
+    elements = elements.length ? elements : [elements];
+    for (var index = 0; index < elements.length; index++) {
+      elements[index].style.display = 'none';
+    }
+  }
+  function show (elements, specifiedDisplay) {
+    elements = elements.length ? elements : [elements];
+    for (var index = 0; index < elements.length; index++) {
+      elements[index].style.display = specifiedDisplay || 'block';
+    }
+  }
+
+function sendmail() {
+    document.getElementById('submitform').classList.add('is-loading');
+    let fullName = document.getElementById("name").value;
+    let userEmail = document.getElementById("email").value;
+    let userMessage = document.getElementById("message").value;
+        var contactParams = {
+            from_name: fullName,
+            from_email: userEmail,
+            message: userMessage
+        };
+        emailjs.send('service_f1mm1r4', 'template_tu8jxt2', contactParams)
+        .then(function(response) {
+            console.log('SUCCESS!', response.status);
+            document.getElementById("email_display").innerHTML = 'Your email has been successfully sent! A response will be submitted to: ' + userEmail;
+            hide(document.getElementById('contactbox'));
+            show(document.getElementById('successemail'));
+         }, function(error) {
+            console.log('FAILED:', error);
+            hide(document.getElementById('contact-form'));
+            show(document.getElementById('failedemail'));
+         });
+}
